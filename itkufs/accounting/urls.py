1e8002494ede57c7357c27e6ca2de4f2924df54e
Stein Magnus Jodal <jodal@samfundet.no>
2007-06-30 16:08:56 +0200

    User URLs defined. Emptry views created.

--- itkufs/accounting/urls.py
+++ itkufs/accounting/urls.py
@@ -2,5 +2,39 @@ from django.conf.urls.defaults import *
 from itkufs.accounting.views import *
 
 urlpatterns = patterns('',
-    url('^$', accounting_view),
+    # Account list
+    url(r'^$',
+        account_list, name='account-list'),
+
+    # My account
+    url(r'^(?P<account_group>[0-9a-z_-]+)/(?P<account>\w+)/$',
+        account_summary, name='account-summary'),
+    url(r'(?P<account_group>[0-9a-z_-]+)/(?P<account>\d+)/deposit/$',
+        transfer, {'transfer_type': 'deposit'}, name='account-deposit'),
+    url(r'(?P<account_group>[0-9a-z_-]+)/(?P<account>\d+)/withdraw/$',
+        transfer, {'transfer_type': 'withdraw'}, name='account-withdraw'),
+    url(r'(?P<account_group>[0-9a-z_-]+)/(?P<account>\d+)/transfer/$',
+        transfer, {'transfer_type': 'transfer'}, name='account-transfer'),
+
+    # Print lists
+    url(r'(?P<account_group>[0-9a-z_-]+)/list/$',
+        list, name='list-select'),
+    url(r'(?P<account_group>[0-9a-z_-]+)/list/internal/$',
+        list, {'list_type': 'internal'}, name='list-internal'),
+    url(r'(?P<account_group>[0-9a-z_-]+)/list/external/$',
+        list, {'list_type': 'external'}, name='list-external'),
+
+
+    # Admin: Account group summary
+    url(r'^(?P<account_group>[0-9a-z_-]+)/$',
+        account_group_summary, name='account-group-summary'),
+
+    # Admin: Transactions
+    # FIXME
+
+    # Admin: Settlements
+    # FIXME
+
+    # Admin: Balance
+    # FIXME
 )
--- itkufs/accounting/views.py
+++ itkufs/accounting/views.py
@@ -4,7 +4,39 @@ from django.shortcuts import render_to_response
 
 from itkufs.accounting.models import *
 
-def accounting_view(request):
-    """Test view"""
+def account_list(request):
+    """Lists the users account groups and accounts, including admin accounts"""
 
-    return render_to_response('base.html', {'admin': True})
+    # FIXME
+    username = 'jodal'
+    user = User.objects.get(username=username)
+    account = user.account_set.all()[0]
+    admin = account.group.admins.filter(username=username).count()
+
+    return render_to_response('accounting/base.html',
+                              {'account': account,
+                               'admin': admin})
+
+def account_group_summary(request, account_group):
+    """Shows a summery for the account group"""
+
+    # FIXME
+    return account_list(request)
+
+def account_summary(request, account_group, account):
+    """Shows a summary for the account"""
+
+    # FIXME
+    return account_list(request)
+
+def transfer(request, account_group, account, transfer_type=None):
+    """Deposit, withdraw or transfer money"""
+
+    # FIXME
+    return account_list(request)
+
+def list(request, account_group, list_type=None):
+    """Print internal and external lists"""
+
+    # FIXME
+    return account_list(request)
