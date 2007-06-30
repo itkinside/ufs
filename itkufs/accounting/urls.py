0480242845e8504edcec910cd26614867b78b81f
Stein Magnus Jodal <jodal@samfundet.no>
2007-07-01 00:14:11 +0200

    Add /a/ between group-slug and account-slug, to avoid collisions with list namespace.

--- itkufs/accounting/urls.py
+++ itkufs/accounting/urls.py
@@ -7,23 +7,23 @@ urlpatterns = patterns('',
         account_list, name='account-list'),
 
     # My account
-    url(r'^(?P<group>[0-9a-z_-]+)/(?P<account>[0-9a-z_-]+)/$',
+    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/$',
         account_summary, name='account-summary'),
-    url(r'^(?P<group>[0-9a-z_-]+)/(?P<account>[0-9a-z_-]+)/(?P<page>\d+)/$',
+    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/(?P<page>\d+)/$',
         account_summary, name='account-summary-page'),
-    url(r'(?P<group>[0-9a-z_-]+)/(?P<account>[0-9a-z_-]+)/deposit/$',
+    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/deposit/$',
         transfer, {'transfer_type': 'deposit'}, name='account-deposit'),
-    url(r'(?P<group>[0-9a-z_-]+)/(?P<account>[0-9a-z_-]+)/withdraw/$',
+    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/withdraw/$',
         transfer, {'transfer_type': 'withdraw'}, name='account-withdraw'),
-    url(r'(?P<group>[0-9a-z_-]+)/(?P<account>[0-9a-z_-]+)/transfer/$',
+    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/transfer/$',
         transfer, {'transfer_type': 'transfer'}, name='account-transfer'),
 
     # Print lists
-    url(r'(?P<group>[0-9a-z_-]+)/list/$',
+    url(r'^(?P<group>[0-9a-z_-]+)/list/$',
         list, name='list-select'),
-    url(r'(?P<group>[0-9a-z_-]+)/list/internal/$',
+    url(r'^(?P<group>[0-9a-z_-]+)/list/internal/$',
         list, {'list_type': 'internal'}, name='list-internal'),
-    url(r'(?P<group>[0-9a-z_-]+)/list/external/$',
+    url(r'^(?P<group>[0-9a-z_-]+)/list/external/$',
         list, {'list_type': 'external'}, name='list-external'),
 
 
