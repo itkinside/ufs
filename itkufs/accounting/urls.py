28989c65a82bc5db9b53ce6fec1afb84a53243a6
Stein Magnus Jodal <jodal@samfundet.no>
2007-07-01 00:12:25 +0200

    Do not require trailing slashes.

--- itkufs/accounting/urls.py
+++ itkufs/accounting/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # My account
     url(r'^(?P<group>[0-9a-z_-]+)/(?P<account>[0-9a-z_-]+)/$',
         account_summary, name='account-summary'),
-    url(r'^(?P<group>[0-9a-z_-]+)/(?P<account>[0-9a-z_-]+)/(?P<page>\d+)$',
+    url(r'^(?P<group>[0-9a-z_-]+)/(?P<account>[0-9a-z_-]+)/(?P<page>\d+)/$',
         account_summary, name='account-summary-page'),
     url(r'(?P<group>[0-9a-z_-]+)/(?P<account>[0-9a-z_-]+)/deposit/$',
         transfer, {'transfer_type': 'deposit'}, name='account-deposit'),
