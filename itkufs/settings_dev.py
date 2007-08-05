87bb1e27ef631608d9951363635d57e0033e87e5
Stein Magnus Jodal <jodal@samfundet.no>
2007-08-05 17:50:51 +0200

    Let dev settings save database at approx same place as prod settings, but relative.

--- itkufs/settings_dev.py
+++ itkufs/settings_dev.py
@@ -5,7 +5,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = 'itkufs.sqlite'             # Or path to database file if using sqlite3.
+DATABASE_NAME = '../itkufs.sqlite'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
