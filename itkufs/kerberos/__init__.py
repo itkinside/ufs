9dff05b78127bd240a5f14c0537dd863fcd1aaef
Stein Magnus Jodal <jodal@samfundet.no>
2007-08-05 15:19:35 +0200

    Review KerberosBackend.

---
+++ itkufs/kerberos/__init__.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+
+class KerberosBackend:
+    def authenticate(self, username=None, password=None):
+        if 'REMOTE_USER' in self.META:
+            username = request.META['REMOTE_USER'].split('@')[0]
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                email = '%s@%s' % (username, settings.MAIL_DOMAIN)
+                user = User(username=username, password=None, email=email)
+                user.save()
+            return user
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
