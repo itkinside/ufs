a2c75a07f10ef85aff15ddb7bc6a8d829fb3f70b
Stein Magnus Jodal <jodal@samfundet.no>
2007-08-05 20:43:19 +0200

    Add django.core.context_processors.request context processor.

--- itkufs/settings_base.py
+++ itkufs/settings_base.py
@@ -70,6 +70,14 @@ TEMPLATE_DIRS = (
     '/home/cassarossa/itk/felles/itkufs/itkufs/templates/',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+)
+
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
