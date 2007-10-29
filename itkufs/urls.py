556c579fb251e9f4920aea46181eda8967e04d29
Stein Magnus Jodal <jodal@samfundet.no>
2007-10-29 21:55:12 +0100

    Remove ufs/ from all paths, as we now are on our own vhost.

--- itkufs/urls.py
+++ itkufs/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('',
-    (r'^ufs/admin/', include('django.contrib.admin.urls')),
-    (r'^ufs/inventory/', include('itkufs.inventory.urls')),
-    (r'^ufs/', include('itkufs.accounting.urls')),
+    (r'^admin/', include('django.contrib.admin.urls')),
+    (r'^inventory/', include('itkufs.inventory.urls')),
+    (r'^', include('itkufs.accounting.urls')),
 
     # Only reached using test server, but always used
     # for reverse lookup of URLs from views and templates
-    url(r'^ufs/media/(?P<path>.*)$', 'django.views.static.serve',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT}, name='media'),
 )
