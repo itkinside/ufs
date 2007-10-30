12dc039ab2ef43dfbe27c929ea09b4fbe61c2faf
Thomas Adamcik <adamcik@samfundet.no>
2007-10-30 16:37:15 +0100

    Refactored js code to live in a object so that we avoid collsions

---
+++ media/js/ufs.js
@@ -0,0 +1,25 @@
+var ufs = {
+  init: function() {
+    ufs.menu();
+  },
+  menu: function () {
+    // Fixes support for our drop-down menu in IE
+    // Taken from http://www.alistapart.com/articles/dropdowns/
+    if (document.all && document.getElementById) {
+      var navRoot = document.getElementById("menulist");
+      for (i=0; i<navRoot.childNodes.length; i++) {
+        var node = navRoot.childNodes[i];
+        if (node.nodeName == "LI") {
+          node.onmouseover = function() {
+            this.className += " over";
+          }
+          node.onmouseout = function() {
+            this.className = this.className.replace(" over", "");
+          }
+        }
+      }
+    }
+  },
+}
+
+window.onload = ufs.init;
