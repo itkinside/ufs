7ccda2f2bfb728a78f5c7e62160c0ff9bffee2a0
Thomas Adamcik <adamcik@samfundet.no>
2007-11-09 02:52:55 +0100

    Start of tests for accounting :D

---
+++ itkufs/accounting/tests.py
@@ -0,0 +1,45 @@
+import unittest
+from itkufs.accounting.models import *
+
+class GroupTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def testDefaultGroup(self):
+        pass
+
+    # ...
+
+class AccountTestCase(unittest.TestCase):
+    def setUp(self):
+        self.group   = Group(name='Group', slug='group')
+        self.default = Account(name='Default', slug='default')
+
+    def testDefaultAccount(self):
+        default = self.default
+
+        self.assertEquals(default.name,'Default')
+        self.assertEquals(default.ignore_block_limit, False)
+        self.assertEquals(default.type, 'Li')
+        self.assertEquals(default.owner, None)
+        self.assertEquals(default.active, True)
+
+    def testAccountTypes(self):
+        pass
+
+    def testAccountWithOwner(self):
+        pass
+
+    def testDisabledAccount(self):
+        pass
+
+    def testAccountBalance(self):
+        pass
+
+
+class TransactionlTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    # ...
+
