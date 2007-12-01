570e2bc9ddeb6e9a5068d00730c3a63357769a4b
Thomas Adamcik <adamcik@samfundet.no>
2007-12-01 20:40:30 +0100

    Added debug template that shows all db queries

--- itkufs/context_processors.py
+++ itkufs/context_processors.py
@@ -3,7 +3,7 @@ from bzrlib.errors import NotBranchError
 
 from django.conf import settings
 
-def bzrRev(request):
+def bzr(request):
     try:
         rev = WorkingTree.open_containing(settings.BZR_BRANCH_DIR)[0].branch.revno()
         return {'BZRREV': rev}
@@ -11,3 +11,14 @@ def bzrRev(request):
         pass
 
     return {}
+
+def debug(request):
+    "Returns context variables helpful for debugging."
+    # Stolen from django.core.context_processors.py, but modified to accept superusers
+    context_extras = {}
+    if settings.DEBUG and (request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS or request.user.is_superuser):
+        context_extras['debug'] = True
+        from django.db import connection
+        context_extras['sql_queries'] = connection.queries
+    return context_extras
+
--- itkufs/settings_prod.py
+++ itkufs/settings_prod.py
@@ -28,7 +28,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
-    'itkufs.context_processors.bzrRev',
+    'itkufs.context_processors.bzr',
+    'itkufs.context_processors.debug',
 )
 
 BZR_BRANCH_DIR = '/home/cassarossa/itk/felles/itkufs/'
