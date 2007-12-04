e0854ff44145644ac4a334e2b11f0dc6f24166f0
Stein Magnus Jodal <jodal@samfundet.no>
2007-12-04 21:38:33 +0100

    Split accounting in accounting and common.

--- itkufs/accounting/tests/views.py
+++ itkufs/accounting/tests/views.py
@@ -8,27 +8,7 @@ class UserViewsTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
-    def testLoginUser(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-    def testGroupList(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-    def testAccountSummary(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
     def testTransfer(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-    def testStaticPage(self):
         """FIXME: Write docstring"""
         # FIXME: Implement test
         self.fail('Test not implemented')
@@ -39,32 +19,12 @@ class AdminViewsTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
-    def testGroupSummary(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-    def testAccountSummary(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
     def testTransfer(self):
         """FIXME: Write docstring"""
         # FIXME: Implement test
         self.fail('Test not implemented')
 
     def testApprove(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-    def testAlterGroup(self): # FIXME: Rename view to edit_group?
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-    def testAlterAccount(self): # FIXME: Rename view to edit_account?
         """FIXME: Write docstring"""
         # FIXME: Implement test
         self.fail('Test not implemented')
--- itkufs/accounting/urls.py
+++ itkufs/accounting/urls.py
@@ -2,25 +2,6 @@ from django.conf.urls.defaults import *
 from itkufs.accounting.views import *
 
 urlpatterns = patterns('',
-    # Login
-    url(r'login/$',
-        login_user, name='login'),
-
-    # Account list
-    url(r'^$',
-        group_list, name='group-list'),
-
-    # Account summary
-    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/$',
-        account_summary, name='account-summary'),
-    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/(?P<page>\d+)/$',
-        account_summary, name='account-summary-page'),
-
-    url(r'^(?P<group>[0-9a-z_-]+)/add/$',
-        alter_account, {'type': 'new'}, name='new-account'),
-    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/edit/$',
-        alter_account, {'type': 'edit'}, name='edit-account'),
-
     # Account actions
     url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/deposit/$',
         transfer, {'transfer_type': 'deposit'}, name='account-deposit'),
@@ -28,20 +9,6 @@ urlpatterns = patterns('',
         transfer, {'transfer_type': 'withdraw'}, name='account-withdraw'),
     url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/transfer/$',
         transfer, {'transfer_type': 'transfer'}, name='account-transfer'),
-
-    # Help
-    url(r'^(?P<group>[0-9a-z_-]+)/help/$',
-        static_page, {'template': 'accounting/help.html'}, name='help'),
-
-    # Group summary
-    url(r'^(?P<group>[0-9a-z_-]+)/$',
-        group_summary, name='group-summary'),
-    url(r'^(?P<group>[0-9a-z_-]+)/(?P<page>\d+)/$',
-        group_summary, name='group-summary-page'),
-
-    # Edit group
-    url(r'^(?P<group>[0-9a-z_-]+)/edit/$',
-        alter_group, name='edit-group'),
 
     # Admin: Transactions
     url(r'^(?P<group>[0-9a-z_-]+)/approve/$',
