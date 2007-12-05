3a376710e0b28cbffd88f9adbab68f0e6a8417fd
Stein Magnus Jodal <jodal@samfundet.no>
2007-12-05 22:54:08 +0100

    Move account/group views back to accounting.

--- itkufs/accounting/urls.py
+++ itkufs/accounting/urls.py
@@ -2,21 +2,19 @@ from django.conf.urls.defaults import *
 from itkufs.accounting.views import *
 
 urlpatterns = patterns('',
-    # Account views
-    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/transaction/$',
-        transaction_list, name='transaction-list'),
-    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/transaction/(?P<transaction>\d+)/$',
-        transaction_details, name='transaction-details'),
+    ### Group
 
-    # User account actions
-    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/deposit/$',
-        transfer, {'transfer_type': 'deposit'}, name='account-deposit'),
-    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/withdraw/$',
-        transfer, {'transfer_type': 'withdraw'}, name='account-withdraw'),
-    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/transfer/$',
-        transfer, {'transfer_type': 'transfer'}, name='account-transfer'),
+    # Summary
+    url(r'^(?P<group>[0-9a-z_-]+)/$',
+        group_summary, name='group-summary'),
+    url(r'^(?P<group>[0-9a-z_-]+)/(?P<page>\d+)/$',
+        group_summary, name='group-summary-page'),
 
-    # Admin group actions
+    # Edit
+    url(r'^(?P<group>[0-9a-z_-]+)/edit/$',
+        edit_group, name='edit-group'),
+
+    # Actions
     url(r'^(?P<group>[0-9a-z_-]+)/approve/$',
         approve, name='approve-transactions'),
     url(r'^(?P<group>[0-9a-z_-]+)/reject/$',
@@ -25,4 +23,33 @@ urlpatterns = patterns('',
         transfer, {'transfer_type': 'register'}, name='register-transactions'),
     url(r'^(?P<group>[0-9a-z_-]+)/multiple/$',
         transfer, {'transfer_type': 'multiple'}, name='multiple-transactions'),
+
+    ### Account
+
+    # Summary
+    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/$',
+        account_summary, name='account-summary'),
+    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/(?P<page>\d+)/$',
+        account_summary, name='account-summary-page'),
+
+    # New and edit
+    url(r'^(?P<group>[0-9a-z_-]+)/add/$',
+        edit_account, {'type': 'new'}, name='new-account'),
+    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/edit/$',
+        edit_account, {'type': 'edit'}, name='edit-account'),
+
+    # User actions
+    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/deposit/$',
+        transfer, {'transfer_type': 'deposit'}, name='account-deposit'),
+    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/withdraw/$',
+        transfer, {'transfer_type': 'withdraw'}, name='account-withdraw'),
+    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/transfer/$',
+        transfer, {'transfer_type': 'transfer'}, name='account-transfer'),
+
+    ### Transactions
+
+    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/transaction/$',
+        transaction_list, name='transaction-list'),
+    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/transaction/(?P<transaction>\d+)/$',
+        transaction_details, name='transaction-details'),
 )
--- itkufs/common/urls.py
+++ itkufs/common/urls.py
@@ -10,28 +10,7 @@ urlpatterns = patterns('',
     url(r'^$',
         login_user, name='index'),
 
-    # Account summary
-    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/$',
-        account_summary, name='account-summary'),
-    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/(?P<page>\d+)/$',
-        account_summary, name='account-summary-page'),
-
-    url(r'^(?P<group>[0-9a-z_-]+)/add/$',
-        edit_account, {'type': 'new'}, name='new-account'),
-    url(r'^(?P<group>[0-9a-z_-]+)/a/(?P<account>[0-9a-z_-]+)/edit/$',
-        edit_account, {'type': 'edit'}, name='edit-account'),
-
     # Help
     url(r'^(?P<group>[0-9a-z_-]+)/help/$',
         static_page, {'template': 'accounting/help.html'}, name='help'),
-
-    # Group summary
-    url(r'^(?P<group>[0-9a-z_-]+)/$',
-        group_summary, name='group-summary'),
-    url(r'^(?P<group>[0-9a-z_-]+)/(?P<page>\d+)/$',
-        group_summary, name='group-summary-page'),
-
-    # Edit group
-    url(r'^(?P<group>[0-9a-z_-]+)/edit/$',
-        edit_group, name='edit-group'),
 )
