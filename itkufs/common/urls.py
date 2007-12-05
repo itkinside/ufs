13b660455e39055c9ab01c8e372e89cf0e2c0d75
Stein Magnus Jodal <jodal@samfundet.no>
2007-12-05 22:55:12 +0100

    Cleanup urlconf.

--- itkufs/common/urls.py
+++ itkufs/common/urls.py
@@ -2,13 +2,11 @@ from django.conf.urls.defaults import *
 from itkufs.common.views import *
 
 urlpatterns = patterns('',
-    # Login
-    url(r'login/$',
-        login_user, name='login'),
-
-    # Account list
+    # Index and login
     url(r'^$',
         login_user, name='index'),
+    url(r'login/$',
+        login_user, name='login'),
 
     # Help
     url(r'^(?P<group>[0-9a-z_-]+)/help/$',
