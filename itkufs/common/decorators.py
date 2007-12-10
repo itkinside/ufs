fc75daa6a5818a4282e74e65cb16418fc845b684
Stein Magnus Jodal <jodal@samfundet.no>
2007-12-10 20:35:02 +0100

    Use asserts in decorators.

--- itkufs/common/decorators.py
+++ itkufs/common/decorators.py
@@ -1,53 +1,54 @@
 from django.utils.translation import ugettext as _, ungettext
 from django.http import HttpResponseForbidden, Http404
 
-from itkufs.accounting.models import *
+from itkufs.accounting.models import Group, Account
 
 def is_group_admin(function):
     def wrapped(request, *args, **kwargs):
-        if 'group' in kwargs:
-            try:
-                group = Group.objects.get(slug=kwargs['group'])
-            except Group.DoesNotExist:
-                raise Http404
+        assert('group' in kwargs)
 
+        try:
+            group = Group.objects.get(slug=kwargs['group'])
+        except Group.DoesNotExist:
+            raise Http404
 
-            if group.admins.filter(id=request.user.id).count():
-                kwargs['is_admin'] = True
-            else:
-                kwargs['is_admin'] = False
-
-            return function(request, *args, **kwargs)
+        if group.admins.filter(id=request.user.id).count():
+            kwargs['is_admin'] = True
         else:
-            raise Exception('You can only decorate methods that take a kwarg group=group.slug')
+            kwargs['is_admin'] = False
+
+        return function(request, *args, **kwargs)
     return wrapped
 
 def limit_to_group(function):
     def wrapped(request, *args, **kwargs):
-        if 'group' in kwargs:
-            try:
-                group = Group.objects.get(slug=kwargs['group'])
-            except Group.DoesNotExist:
-                raise Http404
+        assert('group' in kwargs)
 
-            if group.account_set.filter(owner=request.user).count() or group.admins.filter(id=request.user.id).count():
-                return function(request, *args, **kwargs)
+        try:
+            group = Group.objects.get(slug=kwargs['group'])
+        except Group.DoesNotExist:
+            raise Http404
 
-        return HttpResponseForbidden(_('You must have an account in this group to be allowed to view this page.'))
-
+        if (group.account_set.filter(owner=request.user).count()
+            or group.admins.filter(id=request.user.id).count()):
+            return function(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden(_('You must have an account in this group to be allowed to view this page.'))
     return wrapped
 
 def limit_to_user(function):
     def wrapped(request, *args, **kwargs):
-        if 'group' in kwargs and 'account' in kwargs:
-            try:
-                group = Group.objects.get(slug=kwargs['group'])
-                account = group.account_set.get(slug=kwargs['account'])
-            except (Group.DoesNotExist, Account.DoesNotExist):
-                raise Http404
+        assert('group' in kwargs)
+        assert('account' in kwargs)
 
-            if account.owner == request.user:
-                return function(request, *args, **kwargs)
+        try:
+            group = Group.objects.get(slug=kwargs['group'])
+            account = group.account_set.get(slug=kwargs['account'])
+        except (Group.DoesNotExist, Account.DoesNotExist):
+            raise Http404
+
+        if account.owner == request.user:
+            return function(request, *args, **kwargs)
 
         return HttpResponseForbidden(_('You must have an account in this group to be allowed to view this page.'))
 
