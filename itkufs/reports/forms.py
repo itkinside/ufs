d039ac27affc6e874d2a27e92b30b1bf6f40b9b7
Thomas Adamcik <adamcik@samfundet.no>
2008-02-07 22:42:45 +0100

    Respect commit value when saving modelforms with overriden save

--- itkufs/reports/forms.py
+++ itkufs/reports/forms.py
@@ -1,6 +1,5 @@
 from django import newforms as forms
 from django.template.defaultfilters import slugify
-from django.utils.translation import ugettext as _
 
 from itkufs.reports.models import *
 from itkufs.common.forms import CustomModelForm
@@ -10,7 +9,33 @@ class ListForm(CustomModelForm):
         model = List
         exclude = ('slug', 'group')
 
+    def save(self, group=None, **kwargs):
+        original_commit = kwargs['commit']
+        kwargs['commit'] = False
+        list = super(ListForm, self).save(**kwargs)
+
+        if not list.slug:
+            list.slug = slugify(list.name)
+        if group:
+            list.group = group
+
+        if original_commit:
+            list.save()
+        return list
+
 class ColumnForm(CustomModelForm):
     class Meta:
         model = ListColumn
         fields = ('name', 'width')
+
+    def save(self, list=None, **kwargs):
+        original_commit = kwargs['commit']
+        kwargs['commit'] = False
+        column = super(ColumnForm, self).save(**kwargs)
+
+        if list:
+           column.group = group
+
+        if original_commit:
+            column.save()
+        return column
