19c02a0447285159c3bfb9ed8bc1aa78714ec121
Stein Magnus Jodal <jodal@samfundet.no>
2008-03-10 23:43:20 +0100

    settings.dev working at my laptop. Ready for easter!

--- itkufs/settings/dev.py
+++ itkufs/settings/dev.py
@@ -4,3 +4,17 @@ from itkufs.settings import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 TEMPLATE_STRING_IF_INVALID = '!!INVALID VAR!!'
+
+# Do not require HTTPS
+SESSION_COOKIE_SECURE = False
+
+# Media
+MEDIA_ROOT = '/home/jodal/projects/itkufs/media/'
+
+# Templates
+TEMPLATE_DIRS = (
+    '/home/jodal/projects/itkufs/itkufs/templates/',
+)
+
+# bzr
+BZR_BRANCH_DIR = '/home/jodal/projects/itkufs/'
