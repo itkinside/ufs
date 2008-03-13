6402713ac0f22cee6b05acb73f9d912cc963b894
Stein Magnus Jodal <jodal@samfundet.no>
2008-03-13 21:27:03 +0100

    Context Processors: Doc cleanup.

--- itkufs/common/context_processors.py
+++ itkufs/common/context_processors.py
@@ -4,8 +4,11 @@ from bzrlib.errors import NotBranchError
 from django.conf import settings
 
 def bzr(request):
+    """Make the bzr rev number available in templates under the name BZRRREV"""
+
     try:
-        rev = WorkingTree.open_containing(settings.BZR_BRANCH_DIR)[0].branch.revno()
+        rev = WorkingTree.open_containing(
+            settings.BZR_BRANCH_DIR)[0].branch.revno()
         return {'BZRREV': rev}
     except (AttributeError, NotBranchError):
         pass
@@ -13,10 +16,18 @@ def bzr(request):
     return {}
 
 def debug(request):
-    "Returns context variables helpful for debugging."
-    # Stolen from django.core.context_processors.py, but modified to accept superusers
+    """
+    Returns context variables helpful for debugging
+
+    Stolen from django.core.context_processors.py, but modified to accept
+    superusers as well as internal IPs
+
+    """
+
     context_extras = {}
-    if settings.DEBUG and (request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS or request.user.is_superuser):
+    if (settings.DEBUG
+        and (request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS
+            or request.user.is_superuser)):
         context_extras['debug'] = True
         from django.db import connection
         context_extras['sql_queries'] = connection.queries
