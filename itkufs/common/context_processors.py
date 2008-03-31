f24f4e5572901177ab6dff43e317b0f7b475a19b
Stein Magnus Jodal <jodal@samfundet.no>
2008-04-01 00:56:09 +0200

    Only just started this one.

--- itkufs/common/context_processors.py
+++ itkufs/common/context_processors.py
@@ -3,6 +3,18 @@ from bzrlib.errors import NotBranchError
 
 from django.conf import settings
 
+def ufs(request):
+    """
+    Returns common µFS variables used in most templates
+
+    The variables are:
+        group and is_admin
+        account and is_owner, if available
+
+    """
+
+    pass # TODO
+
 def bzr(request):
     """Make the bzr rev number available in templates under the name BZRRREV"""
 
