b621f533a1724774fc1215f663b2a4c67e9f1179
Thomas Adamcik <adamcik@samfundet.no>
2008-04-07 21:05:19 +0200

    Add user_transaction flag to indicate that a user can cancel the transaction

--- itkufs/accounting/models.py
+++ itkufs/accounting/models.py
@@ -399,6 +399,7 @@ class Transaction(models.Model):
     last_modified = models.DateTimeField(_('Last modified'), auto_now_add=True)
     state = models.CharField(_('state'), max_length=3,
         choices=TRANSACTION_STATE, blank=True)
+    user_transaction = models.BooleanField(default=True)
 
     class Meta:
         verbose_name = _('transaction')
