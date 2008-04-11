c69396536a389394be824c853325b7cdb7a7d559
Thomas Adamcik <adamcik@samfundet.no>
2008-04-12 00:46:50 +0200

    Update tag to use entry instead of account

--- itkufs/common/templatetags/ufs.py
+++ itkufs/common/templatetags/ufs.py
@@ -7,22 +7,23 @@ register = Library()
 def do_hide(parser, token):
     try:
         # split_contents() knows not to split quoted strings.
-        tag_name, account, value = token.split_contents()
+        tag_name, entry, value = token.split_contents()
     except ValueError:
         raise TemplateSyntaxError, "%r tag requires exactly two arguments" % token.contents.split()[0]
     if value[0] == value[-1] and value[0] in ('"', "'"):
         raise TemplateSyntaxError, "%r tag only takes variables" % tag_name
 
-    return HideNode(account, value)
+    return HideNode(entry, value)
 
 class HideNode(Node):
-    def __init__(self, account, value):
+    def __init__(self, entry, value):
         self.value = Variable(value)
-        self.account = Variable(account)
+        self.entry = Variable(entry)
 
     def render(self, context):
         # FIXME this needs to be documentet and explainded alot better
-        account = self.account.resolve(context)
+        # FIXME store transaction with select related to reduce query count
+        entry = self.entry.resolve(context)
         value = self.value.resolve(context)
 
         try:
@@ -33,11 +34,13 @@ class HideNode(Node):
 
         if context.get('is_admin', False):
             show = True
-        elif context.get('user_account', None) == account:
+        elif context.get('user_account', None) == entry.account:
             show = True
         elif not decimal and not context['group'].admin_only:
             show = True
-        elif not decimal and not account.is_user_account():
+        elif not decimal and not entry.account.is_user_account():
+            show = True
+        elif entry.transaction.user_transaction:
             show = True
         else:
             show = False
