a9053f875184b4e028cca14ca49e563b90dbaffc
Stein Magnus Jodal <jodal@samfundet.no>
2008-04-17 22:08:55 +0200

    Comment out non-implemented tests, as they just helps to hide failing existing tests.

--- itkufs/accounting/tests/views.py
+++ itkufs/accounting/tests/views.py
@@ -2,76 +2,76 @@ import unittest
 
 from itkufs.accounting.views import *
 
-class UserViewsTestCase(unittest.TestCase):
-    """Tests the views as an unprivileged user"""
-
-    # FIXME: Add missing view tests
-
-    def setUp(self):
-        pass
-
-    # Account views
-
-    def testAccountSummary(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-    # Group views
-
-    def testGroupSummary(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-    # Transaction views
-
-    def testTransfer(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-class AdminViewsTestCase(unittest.TestCase):
-    """Tests the views as a group admin"""
-
-    # FIXME: Add missing view tests
-
-    def setUp(self):
-        pass
-
-    # Account views
-
-    def testAccountSummary(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-    def testEditAccount(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-    # Group views
-
-    def testGroupSummary(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-    def testEditGroup(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-    # Transaction views
-
-    def testTransfer(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
-    def testApprove(self):
-        """FIXME: Write docstring"""
-        # FIXME: Implement test
-        self.fail('Test not implemented')
-
+#class UserViewsTestCase(unittest.TestCase):
+#    """Tests the views as an unprivileged user"""
+#
+#    # FIXME: Add missing view tests
+#
+#    def setUp(self):
+#        pass
+#
+#    # Account views
+#
+#    def testAccountSummary(self):
+#        """FIXME: Write docstring"""
+#        # FIXME: Implement test
+#        self.fail('Test not implemented')
+#
+#    # Group views
+#
+#    def testGroupSummary(self):
+#        """FIXME: Write docstring"""
+#        # FIXME: Implement test
+#        self.fail('Test not implemented')
+#
+#    # Transaction views
+#
+#    def testTransfer(self):
+#        """FIXME: Write docstring"""
+#        # FIXME: Implement test
+#        self.fail('Test not implemented')
+#
+#class AdminViewsTestCase(unittest.TestCase):
+#    """Tests the views as a group admin"""
+#
+#    # FIXME: Add missing view tests
+#
+#    def setUp(self):
+#        pass
+#
+#    # Account views
+#
+#    def testAccountSummary(self):
+#        """FIXME: Write docstring"""
+#        # FIXME: Implement test
+#        self.fail('Test not implemented')
+#
+#    def testEditAccount(self):
+#        """FIXME: Write docstring"""
+#        # FIXME: Implement test
+#        self.fail('Test not implemented')
+#
+#    # Group views
+#
+#    def testGroupSummary(self):
+#        """FIXME: Write docstring"""
+#        # FIXME: Implement test
+#        self.fail('Test not implemented')
+#
+#    def testEditGroup(self):
+#        """FIXME: Write docstring"""
+#        # FIXME: Implement test
+#        self.fail('Test not implemented')
+#
+#    # Transaction views
+#
+#    def testTransfer(self):
+#        """FIXME: Write docstring"""
+#        # FIXME: Implement test
+#        self.fail('Test not implemented')
+#
+#    def testApprove(self):
+#        """FIXME: Write docstring"""
+#        # FIXME: Implement test
+#        self.fail('Test not implemented')
+#
