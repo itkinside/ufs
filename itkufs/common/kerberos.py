840e5f8c47752d06c9cbb5e9bf125a52369a0707
Thomas Adamcik <adamcik@samfundet.no>
2008-12-13 17:03:38 +0100

    Update kerberos backend so that it has a create_user function

--- itkufs/common/kerberos.py
+++ itkufs/common/kerberos.py
@@ -1,3 +1,5 @@
+from pwd import getpwnam
+
 from django.conf import settings
 from django.contrib.auth.models import User
 
@@ -5,14 +7,9 @@ class KerberosBackend:
     def authenticate(self, request=None):
         if request and 'REMOTE_USER' in request.META:
             username = request.META['REMOTE_USER'].split('@')[0]
-            try:
-                user = User.objects.get(username=username)
-            except User.DoesNotExist:
-                email = '%s@%s' % (username, settings.MAIL_DOMAIN)
-                user = User(username=username, password='In Kerberos')
-                user.email = email
-                user.save()
-            return user
+
+            return self.create_user(username)
+
         return None
 
     def get_user(self, user_id):
@@ -20,3 +17,19 @@ class KerberosBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+    def create_user(self, username):
+        try:
+            getpwnam(username)
+        except KeyError:
+            return None
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            email = '%s@%s' % (username, settings.MAIL_DOMAIN)
+            user = User(username=username, password='In Kerberos')
+            user.email = email
+            user.save()
+
+        return user
