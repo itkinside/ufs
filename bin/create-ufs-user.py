ce3e5ac25d5bb5963aa9bf11de1afa53c4ba2cbf
Thomas Adamcik <adamcik@samfundet.no>
2009-03-15 14:47:12 +0100

    Print how new users can be created instead of hiding it in a comment

--- bin/create-ufs-user.py
+++ bin/create-ufs-user.py
@@ -1,2 +1,3 @@
-# Replaced by itkufs/accounting/management/commands/createuser.py
-# which may be used by running `./manage.py createuser`.
+#!/usr/bin/python
+print "Replaced by itkufs/accounting/management/commands/createuser.py"
+print "which may be used by running `./manage.py createuser`."
