04c947e34e35a0b817e442fe91e2f27569843abe
Thomas Adamcik <adamcik@samfundet.no>
2009-03-21 00:45:38 +0100

    Cleanup sort method

--- itkufs/common/utils.py
+++ itkufs/common/utils.py
@@ -14,8 +14,15 @@ def callsign_sorted(objects):
     return sorted_list
 
 def callsign_key(value):
-    return (locale.strxfrm(value.name.lower().encode('utf-8')),
-            value.short_name)
+    is_callsign = value.short_name and CALLSIGN_RE.match(value.short_name)
+
+    return {
+        'name': locale.strxfrm(value.name.lower().encode('utf-8')),
+        'short_name': value.short_name or '',
+        'is_callsign': is_callsign,
+        'is_la': is_callsign and value.short_name.startswith('LA'),
+        'is_lb': is_callsign and value.short_name.startswith('LB'),
+    }
 
 def callsign_cmp(x, y):
     '''
@@ -27,36 +34,24 @@ def callsign_cmp(x, y):
             4. Other names
     '''
 
-    x_short_name = x[1] or ''
-    y_short_name = y[1] or ''
-
-    x_is_callsign = CALLSIGN_RE.match(x_short_name)
-    y_is_callsign = CALLSIGN_RE.match(y_short_name)
-
-    if x_is_callsign and y_is_callsign:
-        x_is_la = x_short_name.startswith('LA')
-        y_is_la = y_short_name.startswith('LA')
-
-        if x_is_la and y_is_la:
-            return cmp(x_short_name, y_short_name)
-        elif x_is_la:
+    if x['is_callsign'] and y['is_callsign']:
+        if x['is_la'] and y['is_la']:
+            return cmp(x['short_name'], y['short_name'])
+        elif x['is_la']:
             return -1
-        elif y_is_la:
+        elif y['is_la']:
             return 1
 
-        x_is_lb = x_short_name.startswith('LB')
-        y_is_lb = y_short_name.startswith('LB')
-
-        if x_is_lb and y_is_lb:
-            return cmp(x_short_name, y_short_name)
-        elif x_is_lb:
+        if x['is_lb'] and y['is_lb']:
+            return cmp(x['short_name'], y['short_name'])
+        elif x['is_lb']:
             return -1
-        elif y_is_lb:
+        elif y['is_lb']:
             return 1
 
-    elif x_is_callsign:
+    elif x['is_callsign']:
         return -1
-    elif y_is_callsign:
+    elif y['is_callsign']:
         return 1
 
-    return cmp(x[0], y[0])
+    return cmp(x['name'], y['name'])
