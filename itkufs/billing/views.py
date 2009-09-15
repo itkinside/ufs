cec4d50514f1bffd262feb32dc2e8f4ec6c54c30
Thomas Adamcik <adamcik@loki>
2009-09-15 19:20:34 +0200

    Fix payment view

--- itkufs/billing/views.py
+++ itkufs/billing/views.py
@@ -9,7 +9,7 @@ from itkufs.common.decorators import limit_to_admin
 from itkufs.accounting.models import RoleAccount, Account, Transaction, TransactionEntry
 from itkufs.billing.models import Bill
 from itkufs.billing.pdf import pdf
-from itkufs.billing.forms import BillingLineFormSet, BillForm
+from itkufs.billing.forms import BillingLineFormSet, BillForm, PaymentForm
 
 @login_required
 @limit_to_admin
@@ -52,7 +52,40 @@ def bill_new_edit(request, group, bill=None, is_admin=False):
 @login_required
 @limit_to_admin
 def bill_payment(request, group, bill, is_admin=False):
-    form = PaymentForm()
+
+    if request.method != 'POST':
+        form = PaymentForm(bill)
+    else:
+        form = PaymentForm(bill, request.POST)
+
+        if form.is_valid():
+            bank = Account.objects.get(group=group,
+                roleaccount__role=RoleAccount.BANK_ACCOUNT)
+
+            other = form.cleaned_data['charge_to']
+
+            settlement = form.cleaned_data.get('settlement', None)
+
+            sum = 0
+            for line in bill.billingline_set.all():
+                sum += line.amount
+
+            transaction = Transaction(group=group, settlement=settlement)
+            transaction.save()
+
+            transaction.entry_set.add(
+                TransactionEntry(account=other, credit=sum))
+            transaction.entry_set.add(
+                TransactionEntry(account=bank, debit=sum))
+
+            transaction.set_pending(user=request.user,
+                message=_('Bill #%s: %s') % (bill.pk, bill.description))
+
+            bill.transaction = transaction
+            bill.save()
+
+            return HttpResponseRedirect(reverse('transaction-details',
+                args=[group.slug,transaction.id]))
 
     return render_to_response('billing/bill_payment.html',
         {
