c38b6a87286b6a6d48e4e7e5defbcba44e1a7088
Thomas Adamcik <adamcik@loki>
2009-09-15 03:24:32 +0200

    Add billing urls

---
+++ itkufs/billing/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+
+from itkufs.billing.views import *
+
+urlpatterns = patterns('',
+    url(r'^(?P<group>[0-9a-z_-]+)/new-bill/$',
+        new_bill, name='new-bill'),
+    url(r'^(?P<group>[0-9a-z_-]+)/billing/$',
+        bill_list, name='bill-list'),
+    url(r'^(?P<group>[0-9a-z_-]+)/billing/(?P<bill>\d+)/$',
+        bill_details, name='bill-details'),
+)
