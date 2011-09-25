df1af78b6a1311f6f251b32706878de42de81dc6
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-09-26 01:45:12 +0200

    Add settings for django-debug-toolbar

--- itkufs/settings/dev.py
+++ itkufs/settings/dev.py
@@ -4,9 +4,10 @@ from itkufs.settings import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-# django-logging
-LOGGING_LOG_SQL = True
-LOGGING_SHOW_METRICS = True
+# django-debug-toolbar
+INSTALLED_APPS += ('debug_toolbar',)
+INTERNAL_IPS = ('127.0.0.1',)
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 
 # Do not require HTTPS
 SESSION_COOKIE_SECURE = False
