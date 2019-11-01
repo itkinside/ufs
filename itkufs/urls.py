6bcce5e86970c15f1494ee35a2c6a53e611d9b19
Stein Magnus Jodal <jodal@samfundet.no>
2019-11-02 00:14:46 +0100

    Make debug mode work without debug_toolbar

--- itkufs/urls.py
+++ itkufs/urls.py
@@ -46,8 +46,11 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [
-        url(r"^__debug__/", include(debug_toolbar.urls))
-    ] + urlpatterns
+    try:
+        import debug_toolbar
+    except ImportError:
+        pass
+    else:
+        urlpatterns = [
+            url(r"^__debug__/", include(debug_toolbar.urls))
+        ] + urlpatterns
