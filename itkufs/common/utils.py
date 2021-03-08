5a216232b22c6443f91d3892ed3bf0c6eee99117
Edgar Vedvik <edgarmv@samfundet.no>
2021-03-08 22:28:16 +0100

    Update callsign sorting

--- itkufs/common/utils.py
+++ itkufs/common/utils.py
@@ -26,9 +26,11 @@ def callsign_sorted(accounts):
     ARK friendly sort method that should sort in the following order:
 
     1. LAxxxx signs
-    2. LBxxxx signs
-    3. xxxxxx signs
-    4. Other names
+    2. LA0xxx signs
+    3. LBxxxx signs
+    4. LB0xxx signs
+    5. xxxxxx signs
+    6. Other names
     """
     return sorted(accounts, key=callsign_key)
 
@@ -36,15 +38,21 @@ def callsign_sorted(accounts):
 def callsign_key(account):
     is_callsign = account.short_name and CALLSIGN_RE.match(account.short_name)
     is_la = is_callsign and account.short_name.startswith("LA")
+    is_la0 = is_callsign and account.short_name.startswith("LA0")
     is_lb = is_callsign and account.short_name.startswith("LB")
+    is_lb0 = is_callsign and account.short_name.startswith("LB0")
 
-    if is_la:
-        index = 1
-    elif is_lb:
+    if is_la0:
         index = 2
-    elif is_callsign:
-        index = 3
-    else:
+    elif is_la:
+        index = 1
+    elif is_lb0:
         index = 4
+    elif is_lb:
+        index = 3
+    elif is_callsign:
+        index = 5
+    else:
+        index = 6
 
     return (index, account.short_name if is_callsign else account.name)
